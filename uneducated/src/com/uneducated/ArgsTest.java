4c07651a70972a01fce409b9dbb5a7ab5a29a52b
geundu <wkdtkvndn@gmail.com>
2022-10-09 12:24:09 +0900

    새 프로젝트, 1차원 어레이 추가

---
+++ uneducated/src/com/uneducated/ArgsTest.java
@@ -0,0 +1,22 @@
+package com.uneducated;
+
+/**
+ * @author geundu
+ * 
+ *         main 메서드 파라미터로 들어오는 args도 스트링타입 어레이.
+ *         프로그램이 실행될 때 받는 옵션, 인자라고 보면 된다
+ *         
+ *         Run configurations... 의 Arguments → Program arguments 에서 추가, 테스트 가능
+ */
+public class ArgsTest {
+    
+    public static void main( String[] args ) {
+        
+        if ( args != null && args.length > 0 ) {
+            
+            for ( int i = 0; i < args.length; i++ ) { System.out.println( args[i] ); }
+        }
+        else
+            System.out.println( "args is empty" );
+    }
+}
---
+++ uneducated/src/com/uneducated/ArrayTest.java
@@ -0,0 +1,40 @@
+package com.uneducated;
+
+/**
+ * @author geundu
+ * 
+ *         1차원 배열을 선언하고 for 루프로 출력해본다
+ *        
+ */
+public class ArrayTest {
+    
+    private static final int ARRAY_LENGTH = 3;
+    
+    public void printIntArray( int[] array ) {
+        System.out.println( "length = " + array.length );
+        
+        for ( int i = 0; i < array.length; i++ ) { System.out.println( "array[" + i + "] = " + array[i] ); }
+        System.out.println( "end of array\n" );
+    }
+    
+    public static void main( String[] args ) {
+        
+        ArrayTest test = new ArrayTest();
+        
+        // new 연산자 사용하여 초기화
+        int[] array0 = new int[10];
+        test.printIntArray( array0 );
+        
+        // new 연산자 사용하여 초기화, 길이에 변수 사용
+        int[] array1 = new int[ARRAY_LENGTH];
+        test.printIntArray( array1 );
+        
+        // 어레이 타입에 맞는 상수를 바로 대입하여 초기화
+        int[] array2 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        test.printIntArray( array2 );
+        
+        // 어레이 타입에 맞는 상수를 바로 대입하여 초기화
+        int[] array3 = new int[] { 0, 1, 2, 3, 4, 5 };
+        test.printIntArray( array3 );
+    }
+}
