26b2707dfbd65f36f542b9c8f6d4de8266f98ae0
geundu <wkdtkvndn@gmail.com>
2022-10-21 23:54:31 +0900

    feat: List에 오브젝트 담아서 출력해보기

---
+++ SampleJavaProject/src/record/Person.java
@@ -0,0 +1,34 @@
+package record;
+
+public class Person {
+    private String name;
+    private int    age;
+    private int    sex;
+    private double height;
+    private double weight;
+    
+    public String getName() { return name; }
+    
+    public void setName( String name ) { this.name = name; }
+    
+    public int getAge() { return age; }
+    
+    public void setAge( int age ) { this.age = age; }
+    
+    public int getSex() { return sex; }
+    
+    public void setSex( int sex ) { this.sex = sex; }
+    
+    public double getHeight() { return height; }
+    
+    public void setHeight( double height ) { this.height = height; }
+    
+    public double getWeight() { return weight; }
+    
+    public void setWeight( double weight ) { this.weight = weight; }
+    
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", age=" + age + ", sex=" + sex + ", height=" + height + ", weight=" + weight + "]";
+    }
+}
---
+++ SampleJavaProject/src/record/PersonList.java
@@ -0,0 +1,48 @@
+package record;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonList {
+    
+    public static void main( String[] args ) {
+        // 데이터셋을 다룰 때 List<객체> 형태의 구조를 아주 많이 사용한다.
+        // List는 엑셀에서 열이라고 보면 되고, <> 안에 들어가는 객체는 행이라고 보면 된다.
+        // 값을 담는 객체라서 보통 VO(Value Object)라고 부름
+        // key : value 쌍으로 이루어진 Map이 들어가기도 한다. List<Map<String, String>> 이런 식으로
+        List<Person> personList = new ArrayList<Person>();
+        
+        // kim 이라는 사람의 정보를 담을 수 있는 클래스 Person을 새로 만들고, 신상정보 입력
+        Person kim = new Person();
+        kim.setName( "KIM" );
+        kim.setAge( 23 );
+        kim.setSex( 0 );
+        kim.setHeight( 180 );
+        kim.setWeight( 70 );
+        
+        // park 이라는 사람의 정보를 담을 수 있는 클래스 Person을 새로 만들고, 신상정보 입력
+        Person park = new Person();
+        park.setName( "PARK" );
+        park.setAge( 25 );
+        park.setSex( 1 );
+        park.setHeight( 165 );
+        park.setWeight( 54 );
+        
+        // 리스트에 객체를 추가한다.
+        personList.add( kim );
+        personList.add( park );
+        
+        // for 루프로 출력해본다.
+        for ( int i = 0; i < personList.size(); i++ ) {
+            System.out.println( personList.get( i ).toString() );
+        }
+        
+        // for-each문 (향상된 for문이라고도 함)
+        for ( Person person : personList ) {
+            System.out.println( person.toString() );
+        }
+        
+        // 이터러블 클래스에서 제공하는 람다식 형태의 for문
+        personList.forEach( p -> System.out.println( p.toString() ) );
+    }
+}
