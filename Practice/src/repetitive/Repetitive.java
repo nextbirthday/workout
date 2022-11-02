345b011d1c30e8b422a523a4318b57eea84c83c3
dmuars <dumarsyoon@gmail.com>
2022-11-02 12:56:03 +0900

    계산기연습

--- Practice/src/repetitive/Repetitive.java
+++ Practice/src/repetitive/Repetitive.java
@@ -1,5 +1,7 @@
 package repetitive;
 
+import java.util.Scanner;
+
 public class Repetitive {
     
     public void testFor() {
@@ -50,19 +52,72 @@ public class Repetitive {
             System.out.println( p++ + "번째 p 출력" );
         }
     }
+    
+    public void testGugudan() {
         
-        public void testGugudan(){
-           
+        for ( int i = 2; i < 10; i++ ) {
+            System.out.println( "=====" + i + "단=====" );
             
-            for(int i = 2; i<10; i++) {
-                System.out.println( "=====" + i + "단=====" );
-                for(int j =1; j<10; j++) {
-                    System.out.println( i+ "X" + j + "=" + i*j );
-                }
+            for ( int j = 1; j < 10; j++ ) {
+                System.out.println( i + "X" + j + "=" + i * j );
+            }
+        }
+    }
+    
+    public void testBreak() {
+        // 숫자를 입력 받아 입력 받은 숫자만큼만 반복적으로 출력 후 종료
+        Scanner sc = new Scanner( System.in );
+        System.out.println( "반복할 횟수 입력:" );
+        int num = sc.nextInt();
+        
+        int i = 1;
+        
+        while ( true ) {
+            System.out.println( i + "번째 i 출력" );
+            
+            if ( i == num ) {
+                break;
+            }
+            i++;
+        }
+        
+    }
+    
+    public void testFor2() {
+        // 초기식 생략 for문
+        int i = 0;
+        
+        for ( ; i < 10; i++ ) {
+            System.out.println( i + "번째 i 출력" );
+        }
+        
+        // 조건식 생략 for 문
+        for ( int j = 0;; j++ ) {
+            
+            if ( j < 10 ) {
+                System.out.println( j + "번째 j 출력" );
+            }
+            else {
+                break;
             }
         }
         
-       
+        // 증감식 생략 for문
+        for ( int p = 0; p < 10; ) {
+            System.out.println( p + "번째 p출력" );
+            p++;
+        }
+        // 모두 생략
+        int l = 0;
+        for(;;) {
+            if(l <10 ) {
+                System.out.println( l +"번째 l 출력" );
+            }else {
+                break;
+            }
+            l++;
+        }
         
     }
-
+    
+}
