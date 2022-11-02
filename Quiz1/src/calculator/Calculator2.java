fdb0477733b2e7d672aa74dff3532feb75a4e5a1
geundu <wkdtkvndn@gmail.com>
2022-11-02 17:33:10 +0900

    feedback: Quiz1 코멘트 작성

--- Quiz1/src/calculator/Calculator2.java
+++ Quiz1/src/calculator/Calculator2.java
@@ -1,43 +1,32 @@
 package calculator;
 
-
 public class Calculator2 {
     
-    int add( int i, int k) {
-        return i + k ;
+    int add( int i, int k ) {
+        return i + k;
     }
     
     int sub( int i, int k ) {
-        return i-k;
-        
+        return i - k;
     }
     
-    int multi( int i, int k) {
-        return i*k;
+    int multi( int i, int k ) {
+        return i * k;
     }
     
-    double div(int i, int k) {
-        return (double)i/k;
+    // i만 double로 캐스팅되기 때문에 0.666...이 출력되는 것
+    // ( double ) ( i / k ) 은 정수끼리 연산 후 캐스팅되기 때문에 소수점은 버려짐
+    double div( int i, int k ) {
+        return ( double ) i / k;
     }
     
-    public static void main(String[] args) {
-        
-        Calculator2 cal = new Calculator2();
-        
-        System.out.println( "합="+cal.add( 0, 0 ) );
-        System.out.println( "차="+cal.sub( 0, 0 ) );
-        System.out.println( "곱="+cal.multi( 0, 0 ) );
-        System.out.println( "나누기="+cal.div( 0, 0 ) );
-        
-    }
-        
-        
-            
+    // public static void main( String[] args ) {
+    //
+    // Calculator2 cal = new Calculator2();
+    //
+    // System.out.println( "합=" + cal.add( 0, 0 ) );
+    // System.out.println( "차=" + cal.sub( 0, 0 ) );
+    // System.out.println( "곱=" + cal.multi( 0, 0 ) );
+    // System.out.println( "나누기=" + cal.div( 0, 0 ) );
+    // }
 }
-   
-    
-    
-    
-    
-    
-
--- Quiz1/src/calculator/Main.java
+++ Quiz1/src/calculator/Main.java
@@ -3,8 +3,12 @@ package calculator;
 public class Main {
     
     public static void main( String[] args ) {
-        Calculator ca = new Calculator();
-        ca.method1();
+        // Calculator ca = new Calculator();
+        // ca.method1();
+        Calculator2 calc = new Calculator2();
+        System.out.println( calc.div( 2, 3 ) );
+        // int 0이 아니라 double 0이기 때문에 무한대가 나오는 것
+        // int 0이면 에러 출력됨
+        System.out.println( calc.div( 8, 0 ) );
     }
-    
 }
