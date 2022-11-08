adf51dc68ad7b138bf0b75b47d52e1c208145b36
geundu <wkdtkvndn@gmail.com>
2022-11-08 23:21:48 +0900

    fix: 베이스볼 게임 로직 수정 및 코멘트 추가

--- SampleJavaProject/src/baseball/Main.java
+++ SampleJavaProject/src/baseball/Main.java
@@ -4,6 +4,6 @@ public class Main {
     
     public static void main( String[] args ) {
         BaseballGameLogic game = new BaseballGameLogic();
-        game.playGame();
+        game.play();
     }
 }
