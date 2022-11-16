4d85d57978dad44ec0dc39a8424651e144967eac
geundu <wkdtkvndn@gmail.com>
2022-11-16 21:46:33 +0900

    feat: 메서드 오버로딩, 오버라이딩 추가

---
+++ SampleJavaProject/src/basic/ParentClass.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class ParentClass {
+    /**
+     * 자식 클래스에서 오버라이딩 될 메서드
+     */
+    protected void parentMethod() {
+        System.out.println( "부모 클래스의 메서드" );
+    }
+}
