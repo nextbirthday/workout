4f473ccbf54215a3c1e610ff5d8a6832e9413acd
dmuars <dumarsyoon@gmail.com>
2022-11-28 17:51:13 +0900

    11월 28일 OT

--- dev_java/src/com/ch01/Sonata.java
+++ dev_java/src/com/ch01/Sonata.java
@@ -1,6 +1,7 @@
 package com.ch01;
 
 public class Sonata {
+    
     // 클래스 선언, Sonata라는 클래스를 정의하였다. 선언하는 이유는 '재사용'
     // 호출할 수 있다.
     // int와 같이 타입을 나타내는 예약어가 있다 - 어디에? JDK설치할 수 있다 -> JDK11
@@ -35,18 +36,28 @@ public class Sonata {
     // 코드 실행시 가장 먼저 호출되는 곳이다.
     // JVM이 자동으로 호출한다.
     
-    public void login( String id, String pw ) {// 파라미터 자리는 선언하는 자리이다.
-        id = "tomato";
-        System.out.println( id ); // 변수 id를 출력
+    public static void login( String id, String pw ) {// 파라미터 자리는 선언하는 자리이다.
+        
+        System.out.println( id + "," + pw ); // 변수 id를 출력//tomato가 출려된다.
+        
     }
     
     // 메인 메소드는 자바에서 제공되는 메소드이다.
-    //그러니까 바꾸면(리턴타입니다 또는 파라미터 타입...) 안된다.
-    // 클론 받아쓰기 X1
+    // 그러니까 바꾸면(리턴타입니다 또는 파라미터 타입...) 안된다.
+    // 클론 받아쓰기 X
     public static void main( String[] args ) {// 이것을 메인메서드라고 한다.
-
+        
         System.out.println( "main 메서드 호출 성공" ); // 1
         methodA();
+        // 메소드를 호출할 때 세미콜론으로 끝내고
+        // 파라미터가 존재하면 파라미터도 고려해야 하고
+        // 또한 타입도 고려되어야만 한다.
+        // 결론: 사용자 정의 메소드 호출 시 파라미터와 타입도 맞춰야 한다.
+        // static 영역에서는 내 안에 정의된 메소드라 하더라도
+        // 반드시 인스턴스화를 하고 호출해야한다.
+        // 두 번째 방법은 methodA();처럼 똑같이 static을 붙여서 선언
+        login( "apple", "123" );
+        login( "tomato", "123" );
         
     }
 }
