7213581785e68115e1c1cc5fa1917adfa7e4279f
geundu <wkdtkvndn@gmail.com>
2022-12-10 14:27:15 +0900

    fix: 업다운게임 코드 수정

--- SampleJavaProject/src/updown/UpDownLogic.java
+++ SampleJavaProject/src/updown/UpDownLogic.java
@@ -21,11 +21,14 @@ public class UpDownLogic {
         boolean isBreak    = false;
         
         while ( !isBreak ) {
-            System.out.print( "0부터 " + MAX + "보다 작은 정수를 입력하세요 : " );
+            System.out.print( "0 이상 " + (MAX - 1) + " 이하 범위의 정수를 입력하세요 : " );
             
             try {
                 userNumber = scan.nextInt();
-                isBreak = true;
+                if ( userNumber < 0 || userNumber >= MAX )
+                    System.out.println( "숫자 입력 범위를 벗어났습니다." );
+                else
+                    isBreak = true;
             }
             catch ( InputMismatchException ime ) {
                 System.out.println( ime.toString() + " : 정수만 입력할 수 있습니다." );
@@ -37,19 +40,16 @@ public class UpDownLogic {
     
     private int compare( int randNumber, int userNumber ) {
         tryCount++;
-        int result = 0;
+        int result = 1;
         
         if ( randNumber == userNumber ) {
             System.out.println( "정답입니다. 도전 횟수 : " + tryCount );
+            result = 0;
         }
-        else if ( randNumber > userNumber ) {
+        else if ( randNumber > userNumber )
             System.out.println( "UP" );
-            result = 1;
-        }
-        else {
+        else
             System.out.println( "DOWN" );
-            result = -1;
-        }
         
         return result;
     }
