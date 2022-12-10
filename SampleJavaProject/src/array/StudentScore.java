961ecf84f37a261552b300471e08be5770050427
geundu <wkdtkvndn@gmail.com>
2022-12-10 20:11:37 +0900

    feat: 성적 구하기 List 샘플

---
+++ SampleJavaProject/src/array/StudentScore.java
@@ -0,0 +1,24 @@
+package array;
+
+public class StudentScore {
+    private String studentName;
+    private double javaScore;
+    private double dbScore;
+    private double linuxScore;
+    
+    public String getStudentName() { return studentName; }
+    
+    public void setStudentName( String studentName ) { this.studentName = studentName; }
+    
+    public double getJavaScore() { return javaScore; }
+    
+    public void setJavaScore( double javaScore ) { this.javaScore = javaScore; }
+    
+    public double getDbScore() { return dbScore; }
+    
+    public void setDbScore( double dbScore ) { this.dbScore = dbScore; }
+    
+    public double getLinuxScore() { return linuxScore; }
+    
+    public void setLinuxScore( double linuxScore ) { this.linuxScore = linuxScore; }
+}
