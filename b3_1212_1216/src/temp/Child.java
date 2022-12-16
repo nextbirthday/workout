a612962ced3f2fb40410dcca0878feb7800a4cf5
geundu <wkdtkvndn@gmail.com>
2022-12-16 18:50:54 +0900

    feat: 간단한 상속 샘플

---
+++ b3_1212_1216/src/temp/Child.java
@@ -0,0 +1,36 @@
+package temp;
+
+public class Child extends Parent {
+    
+    private int childVar;
+    
+    @Override
+    protected void parentMethod() {
+        System.out.println( "Override parentMethod" );
+    }
+    
+    private void childMethod() {
+        System.out.println( "childMethod" );
+    }
+    
+    public static void main( String[] args ) {
+        /*
+         * 인스턴스는 Child 타입이지만 부모 타입 변수에 주소를 저장한다.
+         * 타입이 Parent이기 때문에 Child에서 선언한 childVar, childMethod() 를 사용할 수 없다.
+         * Child 타입의 멤버를 사용하려면 다운캐스팅을 해주면 된다.
+         */
+        Parent p = new Child();
+        System.out.println( p + ", parentVar : " + p.parentVar );
+        // 아래 두 라인은 에러 발생
+        // p.childVar;
+        // p.childMethod();
+        
+        Child c = ( Child ) p;
+        // 변수의 타입이 바뀌는 거지 주소가 바뀌는 것이 아니므로 주솟값은 동일하다.
+        System.out.println( c + ", childVar : " + c.childVar );
+        
+        // 메서드는 오버라이드 된 메서드가 호출된다.
+        p.parentMethod();
+        c.childMethod();
+    }
+}
---
+++ b3_1212_1216/src/temp/Parent.java
@@ -0,0 +1,9 @@
+package temp;
+
+public class Parent {
+    protected int parentVar;
+    
+    protected void parentMethod() {
+        System.out.println( "parentMethod" );
+    }
+}
