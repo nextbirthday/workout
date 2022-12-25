fe2c2d98e82e778d1d11f33e6f53bb1146add4b9
dmuars <dumarsyoon@gmail.com>
2022-12-25 11:55:53 +0900

    laptop commit

--- Practice/src/variable/Run.java
+++ Practice/src/variable/Run.java
@@ -5,9 +5,9 @@ public class Run {
     public static void main( String[] args ) {
         
         VariablePractice vp = new VariablePractice();
-        // vp.method1();
-        // vp.method2();
-        // vp.method3();
+        vp.method1();
+        vp.method2();
+        vp.method3();
         vp.method4();
     }
     
